f31c70aae67390edb6a2d5c74000fce3af9687f2
osicodesinc <osicodesinc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 10:44:02 +0000

    updated text in various areas
    git-svn-id: https://plugins.svn.wordpress.org/php-live-wordpress/trunk@1207403 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- phplive.php
+++ phplive.php
@@ -6,7 +6,7 @@
 	Description: PHP Live! plugin enables PHP Live! integration with your WordPress website.
 	Author: osicodesinc
 	Author URI: http://www.phplivesupport.com
-	Version: 1.7
+	Version: 1.9
 	*/
 
 	/* (c) OSI Codes Inc. */
